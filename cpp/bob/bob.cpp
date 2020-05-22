aa3787777f33483c90c16e6344a41d3f704c2139
heitor <heitorleite.dev@gmail.com>
2020-05-21 22:39:32 -0300

    add bob solution

---
+++ cpp/bob/bob.cpp
@@ -0,0 +1,44 @@
+#include "bob.h"
+#include <iostream>
+
+using namespace std;
+
+namespace bob {
+  string hey(const string str) {
+    bool yelling, empty = true;
+    bool hasalpha = false;
+    char last;
+
+    for (char c : str) {
+      if (isalpha(c))
+        hasalpha = true;
+
+      if (c != toupper(c))
+        yelling = false;
+
+      if (isgraph(c)) {
+        empty = false;
+        last = c;
+      }
+    }
+
+    yelling = yelling && hasalpha;
+
+    bool question = last == '?';
+
+    if (empty)
+      return "Fine. Be that way!";
+
+    if (question) {
+      if (yelling)
+        return "Calm down, I know what I'm doing!";
+
+      return "Sure.";
+    }
+    
+    if (yelling)
+      return "Whoa, chill out!";
+    
+    return "Whatever.";
+  }
+}
